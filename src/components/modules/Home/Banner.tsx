d922d3f0d2a3f19cef0dcf45baa3855416af83e4
Md Sujon Mia <sujon258549@gmail.com>
2025-02-22 00:34:39 +0600

    changed 6 files

--- src/components/modules/Home/Banner.tsx
+++ src/components/modules/Home/Banner.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import bannerPng from "../../../assets/Banner/";
+import bannerPng from "../../../assets/Banner/bannerpng.png";
 const Banner = () => {
   return (
     <div>
@@ -11,16 +11,17 @@ const Banner = () => {
               'linear-gradient(to bottom,  rgba(255, 255, 255, 0), #ffffffbe), url("/bannerbg2.png"), url("/banner.png")',
             backgroundSize: "cover",
             backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            height: "100vh", // Optional: Full viewport height
+            backgroundRepeat: "no-repeat", // Optional: Full viewport height
           }}
-          className="grid rounded-md md:px-10  px-4 mx-auto lg:gap-8 xl:gap-0  lg:grid-cols-12"
+          className=" rounded-md lg:h-[516px] md:px-10 lg:flex-row flex-col-reverse px-8 mx-auto py-10 lg:py-0 lg:flex gap-5 items-center md:grid-cols-2"
         >
-          <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="  md:text-4xl text-3xl lg:text-5xl font-extrabold">
-              Don,t Miss Out on <br /> These Unbeatable Black <br /> Friday
-              Deals!
+          <div className=" lg:w-[60%]">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-extrabold">
+              Don't Miss Out on <br />
+              <span className="block my-3">These Unbeatable Black</span>
+              Friday Deals!
             </h1>
+
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 pt-5">
               From checkout to global sales tax compliance, companies around the
               world use Flowbite to simplify their payment stack.
@@ -32,8 +33,13 @@ const Banner = () => {
               All Products
             </Button>
           </div>
-          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <Image width={250} height={250} src={} />
+          <div className="lg:w-[40%] relative   md:mt-0 lg:-mt-5 h-[300px] md:h-[500px]">
+            <Image
+              src={bannerPng}
+              alt="banner-png"
+              className="object-cover"
+              fill
+            />
           </div>
         </div>
       </section>
