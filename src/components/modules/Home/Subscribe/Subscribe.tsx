3c69a783fddf250a72e1b6a5de2d62adf736b3b9
Md Sujon Mia <sujon258549@gmail.com>
2025-02-28 02:31:54 +0600

    changed 5 files

--- src/components/modules/Home/Subscribe/Subscribe.tsx
+++ src/components/modules/Home/Subscribe/Subscribe.tsx
@@ -1,24 +1,25 @@
 import { Button } from "@/components/ui/button";
-
+import Image from "next/image";
 import styles from "../Banner/banner.module.css";
 const Subscribe = () => {
   return (
     <div>
       <section className="my-10">
         <div className={`${styles.banner}`}>
-          <div className="bg-[url('/man.png')] bg-cover bg-top-left bg-no-repeat ">
-            {/* <div className={`${styles.subscribe}`}> */}
-            <div className="rounded-md lg:h-[516px] md:px-10 px-8 mx-auto py-10 lg:py-0 flex flex-col lg:flex-row-reverse  gap-5 items-center">
+          <div className={`${styles.subscribe}`}>
+            <div className="rounded-md lg:h-[516px] md:px-10 px-8  py-10  lg:py-0 flex flex-col lg:flex-row-reverse  gap-5 items-center">
               {/* Right Image (Appears First on Mobile) */}
-              <div className="w-full lg:w-[30%] relative  lg:-mt-5 h-[400px] md:h-[500px]"></div>
 
               {/* Left Content */}
-              <div className="w-full lg:w-[70%]">
-                <h1 className="text-3xl lg:block hidden md:text-4xl lg:text-5xl font-extrabold">
+              <div className="w-full  md:py-10 lg:py-0">
+                <h1 className="text-3xl hidden lg:block md:text-4xl lg:text-5xl font-extrabold">
                   <span className="block my-3">
                     Stay Update with Exclusive{" "}
                   </span>
                   Offers
+                </h1>
+                <h1 className="text-3xl text-center md:text-left lg:hidden block md:text-4xl lg:text-5xl font-extrabold">
+                  Stay Update with Exclusive Offers
                 </h1>
 
                 <p className="max-w-2xl lg:text-left text-center mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 pt-5">
@@ -27,13 +28,21 @@ const Subscribe = () => {
                 </p>
 
                 {/* Buttons */}
-                <div className="flex flex-wrap gap-3">
-                  <Button className="inline-flex w-full md:w-32 items-center rounded-full hover:text-black text-white justify-center px-5 py-3 text-base font-medium border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
-                    Buy Now
-                  </Button>
-                  <Button className="inline-flex w-full md:w-32 rounded-full items-center hover:text-white justify-center px-5 py-3 text-base font-medium border border-gray-300 bg-transparent hover:bg-customcolor text-black focus:ring-4 focus:ring-gray-100">
-                    All Products
-                  </Button>
+                <div className="">
+                  <form>
+                    <div className="flex md:justify-start justify-center mx-auto md:mx-0 gap-3 max-w-md ">
+                      <input
+                        type="text"
+                        id="first_name"
+                        className="bg-white border rounded-full border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Enter Your Email"
+                        required
+                      />
+                      <Button className="inline-flex md:w-32 items-center rounded-full hover:text-black text-white justify-center  text-base font-medium  focus:ring-4 focus:ring-gray-100">
+                        Subscribe
+                      </Button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
