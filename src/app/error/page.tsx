293aa6745a4c638a23aa2d499f9fca92c796c97e
Md Sujon Mia <sujon258549@gmail.com>
2025-03-17 21:47:21 +0600

    modified page.tsx added Error.tsx

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Error from "@/components/modules/Error/Error";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div>
+      <Error />
+    </div>
+  );
 };
 
 export default ErrorPage;
